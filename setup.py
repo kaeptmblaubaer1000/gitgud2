814564104a0a2a303dffa2a25cd437dd5963bc05
Kaeptm Blaubaer <kaeptmblaubaer1000@gmail.com>
2019-02-13 18:11:59 +0100

    Update setup.py to match fork

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, Extension, find_packages
 
-setup(name='gitgud',
-      version='1.1',
-      author='Filip Sufitchi',
-      author_email="fsufitchi@gmail.com",
+setup(name='gitgud2',
+      version='2.0',
+      author='Kaeptm Blaubaer',
+      author_email=__import__('base64').b64decode(''.join(map(lambda c1, c2: chr(c1^c2), *zip((17611, 17578), (8271, 8317), (33432, 33502), (15455, 15411), (64937, 64970), (58915, 58987), (61898, 61848), (49756, 49704), (27519, 27430), (12302, 12387), (63944, 63920), (3715, 3819), (51093, 51185), (56723, 56772), (276, 350), (58377, 58465), (34908, 34822), (29984, 30072), (13399, 13342), (41606, 41726), (4009, 4068), (2925, 2857), (3335, 3398), (1206, 1217), (49965, 50044), (28390, 28321), (55327, 55419), (3806, 3754), (29057, 29144), (57394, 57445), (64987, 64951), (30550, 30501), (45311, 45235), (30260, 30297), (28676, 28746), (60241, 60199), (37982, 37948), (2816, 2897), (54549, 54568), (13107, 13070))))),
       description="Git Gud - a utility for when you are told to 'get good'",
-      url="https://github.com/fsufitch/git-gud",
+      long_description="Git Gud - a utility for when you are told to 'get good', forked from https://github.com/fsufitch/git-gud"
+      url="https://github.com/kaeptmblaubaer1000/gitgud2",
       package_dir={'':'src'},
       packages=['gitgud'],
+      provides=['gitgud==1.1'],
       entry_points = {
           "console_scripts": [
               "git-gud=gitgud.gitgud:git.gud",
@@ -19,4 +21,4 @@ setup(name='gitgud',
         },
 
       install_requires=['argparse', 'pyfiglet'],
-      )
+)
